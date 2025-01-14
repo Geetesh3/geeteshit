bcf082ab11827a00d265e7ea9fea9e04974a0835
Geetesh Workspace <98798332+Geetesh3@users.noreply.github.com>
2025-01-14 13:14:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// alert("You are now entering to the leading IT solution company. Please wait....");
+function display_mob_menu(){
+const p_menu=querySelector(".p_menu");
+p_menu.classList.toggle('active');
+}
